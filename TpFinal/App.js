853df686cf1a9e88f8fe0e23b41b15543e711c98
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-11-23 10:42:17 -0300

    ashee

--- TpFinal/App.js
+++ TpFinal/App.js
@@ -1,30 +1,38 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View } from 'react-native';
 import SoundPlayer from './TrackPlayer/SoundPlayer';
 import CardVideo from './Components/CardVideo';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom';
+import Camara from './Components/Camara';
+import VibracionScreen from './Components/Vibracion';
 
 export default function App() {
   return (
-    <>
     <BrowserRouter>
-      <Routes>
-        <Route index element={<Home />}></Route>
-        <Route path='*' element={<h1>Not Found</h1>} ></Route>
-      </Routes>
-    </BrowserRouter>
-    <View>
-      <h1>5IA proyectos remix</h1>
       <View>
-        <CardVideo
-          title="Título del video"
-          description="Descripción del video. Puedes agregar más detalles aquí."/>
+        <h1>5IA proyectos remix</h1>
+        <View>
+          <Link to="/video">Ver Video</Link>
+        </View>
+        <View>
+          <Link to="/sound">Reproducir Sonido</Link>
+        </View>
+        <View>
+          <Link to = "/camara">Abrir Camara</Link>
+        </View>
+        <View>
+          <Link to = "/vibracion">Vibracion</Link>
+        </View>
+
+
+        <Routes>
+          <Route path="/video" element={<CardVideo title="Título del video" description="Descripción del video. Puedes agregar más detalles aquí." />} />
+          <Route path="/sound" element={<SoundPlayer />} />
+          <Route path= "/camara" element ={<Camara/>}/>
+          <Route path="/vibracion" element={<VibracionScreen />} />
+          <Route path="/" element={<Navigate to="/video" />} />
+        </Routes>
       </View>
-      <View >
-        <SoundPlayer />
-      </View>
-    </View>
-    </>
+    </BrowserRouter>
   );
 }
